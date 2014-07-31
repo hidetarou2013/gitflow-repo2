a42c2e3631b0cc0c381a731faf154cdf66cc0917
vm72-maekawa <hide1227@gmail.com>
2014-07-31 09:06:18 +0900

    commit&push vm72 eclipse4.3 myfeat4

---
+++ src/test/java/jp/co/eit/kenshuu/gitflow/repo2/HelloMyFeat4Test.java
@@ -0,0 +1,48 @@
+/**
+ *
+ */
+package jp.co.eit.kenshuu.gitflow.repo2;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author Administrator
+ *
+ */
+public class HelloMyFeat4Test {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	/**
+	 * {@link jp.co.eit.kenshuu.gitflow.repo2.HelloMyFeat1#method01(int)} のためのテスト・メソッド。
+	 */
+	@Test
+	public void testMethod01() {
+		fail("まだ実装されていません"); // TODO
+	}
+
+	/**
+	 * {@link jp.co.eit.kenshuu.gitflow.repo2.HelloMyFeat1#method02(int)} のためのテスト・メソッド。
+	 */
+	@Test
+	public void testMethod02() {
+		fail("まだ実装されていません"); // TODO
+	}
+
+}
